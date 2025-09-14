901bbde02c98e7d7f86689242a489cfe0013f920
K.M Ahnaf Zamil <ahnaf@ahnafzamil.com>
2025-09-14 22:46:33 +0600

    chore: refactor encoder code into separate .cpp and .hpp files
    chore: add build instructions to README

--- README.md
+++ README.md
@@ -9,11 +9,44 @@ A backend system inspired by YouTube’s upload flow. It allows users to upload 
 5. S3/MinIO (for video storage)
 6. Docker (for containerization)
 7. PostgreSQL (for storing video information)
-8. React (for the web frontend)
+8. React+CRA (for the web frontend, will migrate to Vite soon)
 
 ## System Design
 ![image](https://github.com/user-attachments/assets/11e3bd72-51fb-4221-aca6-193a57419c88)
 
+
+## Running the Project
+
+### Encoder
+
+This project uses Meson and Ninja to build the C++ program with the GCC compiler.
+
+Use the following commands to build and run the encoder
+```bash
+cd encoder
+meson setup build # setup the build files
+ninja -C build # compile the program
+
+# make sure to create the .env file and set up the credentials for first run
+cp .env.example .env
+
+# run the program
+./build/encoder
+```
+
+### Web UI
+
+Make sure you have `yarn` and Node.js installed.
+
+```
+cd web
+yarn # to install dependencies
+yarn start # start the react server
+```
+
+### REST API
+
+Instructions WIP.
 
 ## Demo
 
--- encoder/src/main.cpp
+++ encoder/src/main.cpp
@@ -1,89 +1,95 @@
 #include <iostream>
-#include <unistd.h>
 #include <signal.h>
+#include <unistd.h>
 
-#include "event2/event.h"
-#include "amqpcpp.h"
+#include "amqpcpp.h" // IWYU pragma: keep
 #include "amqpcpp/libevent.h"
 #include "dotenv.h"
+#include "event2/event.h"
 
 #include "consumer.hpp"
 
 #define ENV_FILE "./.env"
+#define QUEUE_NAME "upload_process"
 
-
-void signal_handler(int signum)
-{
-    std::cout << "Stopping" << std::endl;
-    exit(0);
+void signal_handler(int signum) {
+  std::cout << "Stopping" << std::endl;
+  exit(0);
 }
 
-void check_and_load_dotenv()
-{
-    // Checking if dotenv exists, loading if it does
-    if (access(ENV_FILE, F_OK) == 0) {
-      std::cout << "Loading dotenv" << std::endl;
-      dotenv::init(ENV_FILE);
-    }
+void check_and_load_dotenv() {
+  // Checking if dotenv exists, loading if it does
+  if (access(ENV_FILE, F_OK) == 0) {
+    std::cout << "Loading dotenv" << std::endl;
+    dotenv::init(ENV_FILE);
+  } else {
+    std::cout << ".env file not found in working directory... Exiting."
+              << std::endl;
+    exit(-1);
+  }
 }
 
-auto start_callback = [](const std::string &consumertag)
-    {
-        // Callback triggered when the consumer starts listening on queue
-      
-      std::cout << "Consumer started" << std::endl;
-    };
-
-class CustomHandler : public AMQP::LibEventHandler
-{
-public:
-    virtual uint16_t onNegotiate(AMQP::TcpConnection *connection, uint16_t interval)
-    {
-        // make sure compilers dont complain about unused parameters
-        (void)connection;
-
-        // default implementation, suggested heartbeat is ok
-        return 0;
-    }
-
-    virtual void onError(AMQP::TcpConnection *connection, const char *message)
-    {
-        std::cerr << message << std::endl;
-        connection->close();
-    }
-
-    using AMQP::LibEventHandler::LibEventHandler;
+auto start_callback = [](const std::string &consumertag) {
+  // Callback triggered when the consumer starts listening on queue
+  std::cout << "MQ: consumer started on queue " << QUEUE_NAME << std::endl;
 };
 
+class CustomHandler : public AMQP::LibEventHandler {
+public:
+  uint16_t onNegotiate(AMQP::TcpConnection *connection, uint16_t interval) {
+    // make sure compilers dont complain about unused parameters
+    (void)connection;
 
-
-int main()
-{  
-    check_and_load_dotenv();
-
-    signal(SIGINT, signal_handler);
-    signal(SIGQUIT, signal_handler);
-
-    auto evbase = event_base_new();
-
-    CustomHandler handler(evbase);
-    Consumer *consumer = new Consumer();
-
-    AMQP::TcpConnection connection(&handler, AMQP::Address(consumer->BROKER_URI));
-
-    AMQP::TcpChannel channel(&connection);
-
-    channel.consume(QUEUE_NAME)
-        .onSuccess(start_callback)
-        .onMessage([&channel,&consumer](const AMQP::Message &message, uint64_t deliveryTag, bool redelivered)
-                   { 
-                        std::string content = message.body();
-                        consumer->handle_message(content.substr(0, message.bodySize()));
-                        channel.ack(deliveryTag); 
-                        return; });
-
-    event_base_dispatch(evbase);
-    event_base_free(evbase);
-
+    // default implementation, suggested heartbeat is ok
     return 0;
+  }
+
+  void onError(AMQP::TcpConnection *connection, const char *message) {
+    std::cerr << "MQ handler error: " << message << std::endl;
+    connection->close();
+  }
+
+  void onReady(AMQP::TcpConnection *connection) {
+    std::cout << "MQ handler: ready state" << std::endl;
+  }
+
+  using AMQP::LibEventHandler::LibEventHandler;
+};
+
+int main() {
+  check_and_load_dotenv();
+
+  signal(SIGINT, signal_handler);
+  signal(SIGQUIT, signal_handler);
+
+  auto evbase = event_base_new();
+
+  CustomHandler handler(evbase);
+  Consumer *consumer = new Consumer();
+
+  AMQP::TcpConnection connection(&handler, AMQP::Address(consumer->BROKER_URI));
+  AMQP::TcpChannel channel(&connection);
+
+  channel.declareQueue(QUEUE_NAME, AMQP::durable);
+
+  // start consuming
+  channel.consume(QUEUE_NAME)
+      .onSuccess(start_callback)
+      .onMessage([&channel, &consumer](const AMQP::Message &message,
+                                       uint64_t deliveryTag, bool redelivered) {
+        std::string content = message.body();
+        consumer->handle_message(content.substr(0, message.bodySize()));
+        channel.ack(deliveryTag);
+        return;
+      });
+
+  // report error
+  channel.onError([](const char *message) {
+    std::cout << "MQ channel error: " << message << std::endl;
+  });
+
+  event_base_dispatch(evbase);
+  event_base_free(evbase);
+
+  return 0;
 }
